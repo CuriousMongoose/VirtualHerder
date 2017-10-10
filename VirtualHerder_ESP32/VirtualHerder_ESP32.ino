1b2f85e86a11693c7c3c93287695e32f1cb0bc7e
DanieCM <danie@curiousmongoose.com>
2017-10-10 23:37:37 +0200

    MPU9250: Removed non-AHRS code, outputs data CSV-like

--- VirtualHerder_ESP32/VirtualHerder_ESP32.ino
+++ VirtualHerder_ESP32/VirtualHerder_ESP32.ino
@@ -27,7 +27,6 @@
 #include "quaternionFilters.h"
 #include "MPU9250.h"
 
-#define AHRS false         // Set to false for basic data read
 #define SerialDebug true  // Set to true to get Serial output for debugging
 
 // Pin definitions
@@ -101,6 +100,10 @@ void setup()
       Serial.println(myIMU.magCalibration[2], 2);
     }
 
+    Serial.println("Data start");
+    Serial.println("Acceleration[mg], Gyroscope[deg/s], Magnetometer [mG]");
+    Serial.println("ax, ay, az, gx, gy, gz, mx, my, mz");
+
   } // if (c == 0x71)
   else
   {
@@ -172,79 +175,31 @@ void loop()
                          myIMU.gy*DEG_TO_RAD, myIMU.gz*DEG_TO_RAD, myIMU.my,
                          myIMU.mx, myIMU.mz, myIMU.deltat);
 
-  if (!AHRS)
+
+  // Serial print and/or display at 0.5 s rate independent of data rates
+  myIMU.delt_t = millis() - myIMU.count;
+
+  if (myIMU.delt_t > 500)
   {
-    myIMU.delt_t = millis() - myIMU.count;
-    if (myIMU.delt_t > 500)
+    if(SerialDebug)
     {
-      if(SerialDebug)
-      {
-        // Print acceleration values in milligs!
-        Serial.print("X-acceleration: "); Serial.print(1000*myIMU.ax);
-        Serial.print(" mg ");
-        Serial.print("Y-acceleration: "); Serial.print(1000*myIMU.ay);
-        Serial.print(" mg ");
-        Serial.print("Z-acceleration: "); Serial.print(1000*myIMU.az);
-        Serial.println(" mg ");
+      Serial.print((int)1000*myIMU.ax); Serial.print(" ,");
+      Serial.print((int)1000*myIMU.ay); Serial.print(" ,");
+      Serial.print((int)1000*myIMU.az); Serial.print(" ,");
 
-        // Print gyro values in degree/sec
-        Serial.print("X-gyro rate: "); Serial.print(myIMU.gx, 3);
-        Serial.print(" degrees/sec ");
-        Serial.print("Y-gyro rate: "); Serial.print(myIMU.gy, 3);
-        Serial.print(" degrees/sec ");
-        Serial.print("Z-gyro rate: "); Serial.print(myIMU.gz, 3);
-        Serial.println(" degrees/sec");
+      Serial.print(myIMU.gx, 2); Serial.print(" ,");
+      Serial.print(myIMU.gy, 2); Serial.print(" ,");
+      Serial.print(myIMU.gz, 2); Serial.print(" ,");
 
-        // Print mag values in degree/sec
-        Serial.print("X-mag field: "); Serial.print(myIMU.mx);
-        Serial.print(" mG ");
-        Serial.print("Y-mag field: "); Serial.print(myIMU.my);
-        Serial.print(" mG ");
-        Serial.print("Z-mag field: "); Serial.print(myIMU.mz);
-        Serial.println(" mG");
+      Serial.print((int)myIMU.mx ); Serial.print(" ,");
+      Serial.print((int)myIMU.my ); Serial.print(" ,");
+      Serial.println((int)myIMU.mz ); //Serial.print(" ,");
 
-        myIMU.tempCount = myIMU.readTempData();  // Read the adc values
-        // Temperature in degrees Centigrade
-        myIMU.temperature = ((float) myIMU.tempCount) / 333.87 + 21.0;
-        // Print temperature in degrees Centigrade
-        Serial.print("Temperature is ");  Serial.print(myIMU.temperature, 1);
-        Serial.println(" degrees C");
-      }
-
-      myIMU.count = millis();
-      digitalWrite(myLed, !digitalRead(myLed));  // toggle led
-    } // if (myIMU.delt_t > 500)
-  } // if (!AHRS)
-  else
-  {
-    // Serial print and/or display at 0.5 s rate independent of data rates
-    myIMU.delt_t = millis() - myIMU.count;
-
-    // update LCD once per half-second independent of read rate
-    if (myIMU.delt_t > 500)
-    {
-      if(SerialDebug)
-      {
-        Serial.print("ax = "); Serial.print((int)1000*myIMU.ax);
-        Serial.print(" ay = "); Serial.print((int)1000*myIMU.ay);
-        Serial.print(" az = "); Serial.print((int)1000*myIMU.az);
-        Serial.println(" mg");
-
-        Serial.print("gx = "); Serial.print( myIMU.gx, 2);
-        Serial.print(" gy = "); Serial.print( myIMU.gy, 2);
-        Serial.print(" gz = "); Serial.print( myIMU.gz, 2);
-        Serial.println(" deg/s");
-
-        Serial.print("mx = "); Serial.print( (int)myIMU.mx );
-        Serial.print(" my = "); Serial.print( (int)myIMU.my );
-        Serial.print(" mz = "); Serial.print( (int)myIMU.mz );
-        Serial.println(" mG");
-
-        Serial.print("q0 = "); Serial.print(*getQ());
-        Serial.print(" qx = "); Serial.print(*(getQ() + 1));
-        Serial.print(" qy = "); Serial.print(*(getQ() + 2));
-        Serial.print(" qz = "); Serial.println(*(getQ() + 3));
-      }
+//      Serial.print("q0 = "); Serial.print(*getQ());
+//      Serial.print(" qx = "); Serial.print(*(getQ() + 1));
+//      Serial.print(" qy = "); Serial.print(*(getQ() + 2));
+//      Serial.print(" qz = "); Serial.println(*(getQ() + 3));
+    }
 
 // Define output variables from updated quaternion---these are Tait-Bryan
 // angles, commonly used in aircraft orientation. In this coordinate system,
@@ -262,39 +217,39 @@ void loop()
 // For more see
 // http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
 // which has additional links.
-      myIMU.yaw   = atan2(2.0f * (*(getQ()+1) * *(getQ()+2) + *getQ() *
-                    *(getQ()+3)), *getQ() * *getQ() + *(getQ()+1) * *(getQ()+1)
-                    - *(getQ()+2) * *(getQ()+2) - *(getQ()+3) * *(getQ()+3));
-      myIMU.pitch = -asin(2.0f * (*(getQ()+1) * *(getQ()+3) - *getQ() *
-                    *(getQ()+2)));
-      myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ()+1) + *(getQ()+2) *
-                    *(getQ()+3)), *getQ() * *getQ() - *(getQ()+1) * *(getQ()+1)
-                    - *(getQ()+2) * *(getQ()+2) + *(getQ()+3) * *(getQ()+3));
-      myIMU.pitch *= RAD_TO_DEG;
-      myIMU.yaw   *= RAD_TO_DEG;
-      // Declination of SparkFun Electronics (40°05'26.6"N 105°11'05.9"W) is
-      // 	8° 30' E  ± 0° 21' (or 8.5°) on 2016-07-19
-      // - http://www.ngdc.noaa.gov/geomag-web/#declination
-      myIMU.yaw   -= 8.5;
-      myIMU.roll  *= RAD_TO_DEG;
 
-      if(SerialDebug)
-      {
-        Serial.print("Yaw, Pitch, Roll: ");
-        Serial.print(myIMU.yaw, 2);
-        Serial.print(", ");
-        Serial.print(myIMU.pitch, 2);
-        Serial.print(", ");
-        Serial.println(myIMU.roll, 2);
+//    myIMU.yaw   = atan2(2.0f * (*(getQ()+1) * *(getQ()+2) + *getQ() *
+//                  *(getQ()+3)), *getQ() * *getQ() + *(getQ()+1) * *(getQ()+1)
+//                  - *(getQ()+2) * *(getQ()+2) - *(getQ()+3) * *(getQ()+3));
+//    myIMU.pitch = -asin(2.0f * (*(getQ()+1) * *(getQ()+3) - *getQ() *
+//                  *(getQ()+2)));
+//    myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ()+1) + *(getQ()+2) *
+//                  *(getQ()+3)), *getQ() * *getQ() - *(getQ()+1) * *(getQ()+1)
+//                  - *(getQ()+2) * *(getQ()+2) + *(getQ()+3) * *(getQ()+3));
+//    myIMU.pitch *= RAD_TO_DEG;
+//    myIMU.yaw   *= RAD_TO_DEG;
+//    // Declination of SparkFun Electronics (40°05'26.6"N 105°11'05.9"W) is
+//    //   8° 30' E  ± 0° 21' (or 8.5°) on 2016-07-19
+//    // - http://www.ngdc.noaa.gov/geomag-web/#declination
+//    myIMU.yaw   -= 8.5;
+//    myIMU.roll  *= RAD_TO_DEG;
 
-        Serial.print("rate = ");
-        Serial.print((float)myIMU.sumCount/myIMU.sum, 2);
-        Serial.println(" Hz");
-      }
+//    if(SerialDebug)
+//    {
+//      Serial.print("Yaw, Pitch, Roll: ");
+//      Serial.print(myIMU.yaw, 2);
+//      Serial.print(", ");
+//      Serial.print(myIMU.pitch, 2);
+//      Serial.print(", ");
+//      Serial.println(myIMU.roll, 2);
+//
+//      Serial.print("rate = ");
+//      Serial.print((float)myIMU.sumCount/myIMU.sum, 2);
+//      Serial.println(" Hz");
+//    }
 
-      myIMU.count = millis();
-      myIMU.sumCount = 0;
-      myIMU.sum = 0;
-    } // if (myIMU.delt_t > 500)
-  } // if (AHRS)
-}
+    myIMU.count = millis();
+    myIMU.sumCount = 0;
+    myIMU.sum = 0;
+  } // if (myIMU.delt_t > 500)
+} //loop()
